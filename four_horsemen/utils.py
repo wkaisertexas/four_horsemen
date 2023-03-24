a7f37247d15b9390a610b45d1c642672baa3fec3
William Kaiser <wkaisertexas@gmail.com>
2023-03-23 23:18:08 -0400

    Cleaned up Utils

--- four_horsemen/utils.py
+++ four_horsemen/utils.py
@@ -1,50 +1,68 @@
-from typing import List
-import requests
+'''
+Utility functions for the four horsemen 
+'''
 import json
 import subprocess
-import os
+from typing import List
+
+import requests
 
 
-def get_posts(subreddit: str, type: str, limit: int, api_key: str):
-    url = f'https://www.reddit.com/r/{subreddit}/{type}.json?limit={limit}'
+def get_posts(subreddit: str, post_type: str, limit: int, api_key: str) -> List[dict]:
+    '''
+    Gets the posts from the subreddit
+    '''
+    url = f'https://www.reddit.com/r/{subreddit}/{post_type}.json?limit={limit}'
 
     print(f'Getting posts from {url}')
     headers = {'User-Agent': 'Mozilla/5.0'}
-    if api_key: 
-        r = requests.get(url, headers=headers, auth=api_key, timeout=20)
+    if api_key:
+        posts = requests.get(url, headers=headers, auth=api_key, timeout=20)
     else:
-        r = requests.get(url, headers=headers, timeout=20)
-    body = json.loads(r.text)
+        posts = requests.get(url, headers=headers, timeout=20)
+    body = json.loads(posts.text)
 
     return list(map(
-               lambda x: x.get('data', {}),
-               body.get('data', {}).get('children', [])
-            )) # Destructures into post-level data
+                lambda x: x.get('data', {}),
+                body.get('data', {}).get('children', []))
+            )
 
 
 def download_image(url: str) -> bool:
     '''Downloads the image from the post'''
 
-    # Starts downloading the image
-    req = requests.get(url, timeout=20)
+    image = requests.get(url, timeout=20)
 
     save_path = f'tmp/{url.split("/")[-1]}'
 
-    if req.status_code != 200:
+    if image.status_code != 200:
         print(f'Failed to download {url}')
         return False
 
-    # Saves the image
-    with open(save_path, 'wb') as f:
-        for chunk in req.iter_content(chunk_size=1024):
+    with open(save_path, 'wb') as file:
+        for chunk in image.iter_content(chunk_size=1024):
             if chunk:
-                f.write(chunk)
+                file.write(chunk)
 
     return True
 
 
-def get_dimensions(post_url: str):
+def get_dimensions(file_path: str):
     '''Gets the dimensions of the post'''
-    output = subprocess.Popen(f'ffprobe -i tmp/{post_url}', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True).communicate()[0].decode('utf-8')
+    output = subprocess.Popen(
+        f'ffprobe -i {file_path}', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+        ).communicate()[0].decode('utf-8')
 
-    return tuple(map(int, output.split('Video:')[1].split(',')[1].split(' ')[1].split('x')))
+    dimensions_line = output.rsplit('Video:', maxsplit=1)[-1].split(',')[2].split(' ')[-1].strip()
+    dimensions_line = dimensions_line[:dimensions_line.find(' ')]
+
+    return tuple(map(int, dimensions_line.split('x')))
+
+def get_length(file_path: str):
+    '''Get the length of a video or audio file'''
+    output = subprocess.Popen(
+        f'ffprobe -i {file_path}', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True
+    ).communicate()[0].decode('utf-8')
+
+    seg = output.split('Duration:')[1].split(',')[0].split(':')
+    return int(seg[0]) * 3600 + int(seg[1]) * 60 + float(seg[2])
